198d339b1e14eecc852e8af4c201c5b75780ec39
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-05-06 22:43:35 +0900

    Initial commit

---
+++ src/main/java/org/sopt/collaboration/melon/MelonServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.collaboration.melon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelonServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sopt/collaboration/melon/MelonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.collaboration.melon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
