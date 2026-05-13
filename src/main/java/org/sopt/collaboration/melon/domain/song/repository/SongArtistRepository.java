b4344d98f05afd3843c73e1eb5a3dd5f5abf2e82
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-05-14 02:19:47 +0900

    HOTFIX: SongArtist Fetch Join 추가

--- src/main/java/org/sopt/collaboration/melon/domain/song/repository/SongArtistRepository.java
+++ src/main/java/org/sopt/collaboration/melon/domain/song/repository/SongArtistRepository.java
@@ -3,8 +3,10 @@ package org.sopt.collaboration.melon.domain.song.repository;
 import java.util.List;
 import org.sopt.collaboration.melon.domain.song.entity.SongArtist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SongArtistRepository extends JpaRepository<SongArtist, Long> {
 
+    @Query("select sa from SongArtist sa join fetch sa.artist where sa.song.id = :songId")
     List<SongArtist> findAllBySongId(Long songId);
 }
